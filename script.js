22b0bfeed9c29580e24f29f4b0e7b045af54ebe6
Srawan Pandey <155293036+sr-wn@users.noreply.github.com>
2025-06-27 17:12:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('header');
-window.addEventListener('scroll', function(){
-header.classList.toggle('sticky', window.scrollY > 130);
-});
-
-let menu=document.querySelector("#menu-icon");
